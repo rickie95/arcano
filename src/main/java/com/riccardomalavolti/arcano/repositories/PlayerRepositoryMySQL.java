1fb76e296f7ae46e0e14e683ecd7df73e368b20c
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-08-21 19:00:29 +0200

    Adds EntityManager support

--- src/main/java/com/riccardomalavolti/arcano/repositories/PlayerRepositoryMySQL.java
+++ src/main/java/com/riccardomalavolti/arcano/repositories/PlayerRepositoryMySQL.java
@@ -2,23 +2,54 @@ package com.riccardomalavolti.arcano.repositories;
 
 import java.util.List;
 
+import javax.enterprise.inject.Default;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import com.riccardomalavolti.arcano.model.Player;
 
-public class PlayerRepo {
-	
-	private List<Player> playerList;
-	
-	public PlayerRepo() {
-		playerList.add(new Player(1, "Mike"));
-		playerList.add(new Player(2, "Jack"));
-	}
+@Default
+public class PlayerRepositoryMySQL implements PlayerRepository {
+
+	@PersistenceContext
+	EntityManager entityManager;
 
 	public List<Player> getAllPlayers() {
-		return playerList;
+		return entityManager.createQuery("SELECT * FROM player;", Player.class).getResultList();
 	}
 
-	public Player getPlayerById(String playerId) {
-		return new Player(1, "Mike");		
+	public Player getPlayerById(Long playerId) {
+		return entityManager.find(Player.class, playerId);
+	}
+
+	@Transactional
+	public boolean commitChanges() {
+
+		Player p1 = new Player();
+
+		p1.setId(Integer.toUnsignedLong(1));
+		p1.setName("Mike");
+
+		Player p2 = new Player();
+		p2.setId(Integer.toUnsignedLong(2));
+		p2.setName("Jack");
+
+		try {
+			entityManager.persist(p1);
+			entityManager.persist(p2);
+		} catch (Exception ex) {
+			System.out.println(ex);
+			entityManager.getTransaction().rollback();
+			return false;
+		}
+		
+		return true;
+	}
+
+	@Override
+	public void addPlayer(Player p) {
+		entityManager.persist(p);
 	}
 
 }
