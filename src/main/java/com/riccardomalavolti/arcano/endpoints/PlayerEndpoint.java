fc36bcb273bd9de3d6f4a6b981f352c0c8c31fd3
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-08-24 23:25:49 +0200

    Adds methods for PUT POST and DELETE to PlayerEndpoint

--- src/main/java/com/riccardomalavolti/arcano/endpoints/PlayerEndpoint.java
+++ src/main/java/com/riccardomalavolti/arcano/endpoints/PlayerEndpoint.java
@@ -1,14 +1,22 @@
 package com.riccardomalavolti.arcano.endpoints;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import com.riccardomalavolti.arcano.model.Player;
 import com.riccardomalavolti.arcano.service.PlayerService;
@@ -37,11 +45,39 @@ public class PlayerEndpoint {
 		return playerService.getAllPlayers();
 	}
 	
-	@Path("/{id}")
 	@GET
+	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Player getPlayer(@PathParam("id") Long id) {
 		return playerService.getPlayerById(id);
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addPlayer(Player p, @Context UriInfo uriInfo) throws URISyntaxException {
+		Player saved = playerService.addPlayer(p);
+		return Response.created(new URI(uriInfo.getAbsolutePath() + "/" + saved.getId()))
+				.entity(saved)
+				.build();
+		
+	}
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Player updatePlayer(@PathParam("id") String id, Player updatedPlayer) {
+		return playerService.updatePlayer(id, updatedPlayer);
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response deletePlayer(@PathParam("id") String id) {
+		return Response
+			.accepted(playerService.deletePlayer(id))
+			.build();
 	}
 
 
