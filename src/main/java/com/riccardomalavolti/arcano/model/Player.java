c2f8021b82044101ac1b80635989d47d8294d64a
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-08-24 23:25:09 +0200

    Moves entity annotations on fields

--- src/main/java/com/riccardomalavolti/arcano/model/Player.java
+++ src/main/java/com/riccardomalavolti/arcano/model/Player.java
@@ -1,33 +1,59 @@
 package com.riccardomalavolti.arcano.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 
 @Entity
-@Table(name="player")
 public class Player {
 	
-	private Long id;
-	private String name;
-	
 	@Id
-	@NotNull
-	@Column(name = "id", nullable = false)
+	@GeneratedValue
+	private Long id;
+	private String username;
+	private String password;
+	
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getName() {
-		return name;
+	public String getUsername() {
+		return username;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Player other = (Player) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 }
