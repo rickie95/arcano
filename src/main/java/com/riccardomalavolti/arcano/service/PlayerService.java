59b3cd442d6c77bfc7808cf5d580f8f4c6c222ba
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-08-26 20:50:39 +0200

    Removes PlayerService interface and makes it concrete with prev imp

--- src/main/java/com/riccardomalavolti/arcano/service/PlayerService.java
+++ src/main/java/com/riccardomalavolti/arcano/service/PlayerService.java
@@ -4,53 +4,48 @@ import java.util.List;
 
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
+import javax.persistence.EntityExistsException;
+import javax.ws.rs.NotFoundException;
 
+import com.riccardomalavolti.arcano.exceptions.ConflictException;
 import com.riccardomalavolti.arcano.model.Player;
-import com.riccardomalavolti.arcano.repositories.PlayerRepositoryMySQL;
+import com.riccardomalavolti.arcano.repositories.PlayerRepository;
 
 @Default
-public class PlayerServiceImp implements PlayerService {
+public class PlayerService {
 	
 	@Inject
-	private PlayerRepositoryMySQL playerRepo;
+	private PlayerRepository playerRepo;
 
-	@Override
-	public void addPlayer(Player p) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void removePlayer(Player p) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void updatePlayer(Player p) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public List<Player> getAllPlayers() {
 		return playerRepo.getAllPlayers();
 	}
 
-	@Override
-	public Player getPlayerById(Long playerId) {
-		return playerRepo.getPlayerById(playerId);
+	public Player getPlayerById(String playerId) {
+		return playerRepo
+				.getPlayerById(Long.parseLong(playerId))
+				.orElseThrow(() -> new NotFoundException("No player exist with id" + playerId));
 	}
 
-	@Override
-	public boolean commitChanges() {
+	public Player addPlayer(Player p) {
 		try {
-		playerRepo.commitChanges();
-		return true;
-		}catch(Exception ex) {
-			System.out.println();
+			return playerRepo.addPlayer(p);
+		}catch(EntityExistsException ex) {
+			throw new ConflictException();
 		}
-		return false;
+	}
+
+	public Player updatePlayer(String id, Player p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Player deletePlayer(String playerId) {
+		Player p = new Player();
+		p.setId(Long.parseLong(playerId));
+		return playerRepo
+				.removePlayer(p)
+				.orElseThrow(() -> new NotFoundException("No player exist with id " + playerId));
 	}
 
 }
