d54ce22588e008a9778d7612bc5a685eec30332e
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-08-27 08:41:29 +0200

    Removes reference to PlayerServiceImp

--- src/test/java/com/riccardomalavolti/arcano/endpoints/BasicServer.java
+++ src/test/java/com/riccardomalavolti/arcano/endpoints/BasicServer.java
@@ -4,12 +4,8 @@ import java.io.IOException;
 import java.net.URI;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-
-import com.riccardomalavolti.arcano.service.PlayerService;
-import com.riccardomalavolti.arcano.service.PlayerServiceImp;
 
 public class BasicServer {
 
@@ -17,16 +13,7 @@ public class BasicServer {
 
 	public static HttpServer startServer() {
 		final ResourceConfig rc = new ResourceConfig()
-				.packages("com.riccardomalavolti.arcano")
-				.register(new AbstractBinder() {
-					
-					@Override
-					protected void configure() {
-						bind(PlayerServiceImp.class)
-							.to(PlayerService.class);
-						
-					}
-				});
+				.packages("com.riccardomalavolti.arcano");
 		
 		return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
 	}
