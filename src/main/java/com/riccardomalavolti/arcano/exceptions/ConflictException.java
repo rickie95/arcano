9acbb8dcf2dbccdaab6279527564261698d8e948
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-08-27 08:45:11 +0200

    Adds ConflictException

---
+++ src/main/java/com/riccardomalavolti/arcano/exceptions/ConflictException.java
@@ -0,0 +1,14 @@
+package com.riccardomalavolti.arcano.exceptions;
+
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.core.Response;
+
+public class ConflictException extends ClientErrorException {
+
+	private static final long serialVersionUID = 6679786088672936532L;
+
+	public ConflictException() {
+		super(Response.Status.CONFLICT);
+	}
+
+}
