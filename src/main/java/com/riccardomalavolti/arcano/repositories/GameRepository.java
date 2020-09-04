47201ba4ae72efc3827d79c259a7342d705c2455
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-09-04 15:40:30 +0200

    Adds GameRepository and updates MatchRepository

---
+++ src/main/java/com/riccardomalavolti/arcano/repositories/GameRepository.java
@@ -0,0 +1,18 @@
+package com.riccardomalavolti.arcano.repositories;
+
+import java.util.List;
+
+import com.riccardomalavolti.arcano.model.Game;
+import com.riccardomalavolti.arcano.model.Player;
+
+public interface GameRepository {
+
+	public Game createGame(Player playerOne, Player playerTwo);
+
+	public Game findGameById(Long gameid);
+
+	public List<Game> findAllGames();
+	
+	public void removeGame(Game game);
+	
+}
---
+++ src/main/java/com/riccardomalavolti/arcano/repositories/MatchRepository.java
@@ -0,0 +1,9 @@
+package com.riccardomalavolti.arcano.repositories;
+
+import com.riccardomalavolti.arcano.model.Match;
+
+public interface MatchRepository {
+
+	Match addMatch(Match m);
+
+}
