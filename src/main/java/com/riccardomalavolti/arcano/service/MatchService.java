ea303115727737def7ccba713118fea1292559fb
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-09-14 12:46:03 +0200

    Updates MatchService

--- src/main/java/com/riccardomalavolti/arcano/service/MatchService.java
+++ src/main/java/com/riccardomalavolti/arcano/service/MatchService.java
@@ -1,18 +1,39 @@
 package com.riccardomalavolti.arcano.service;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.NotFoundException;
+
 import com.riccardomalavolti.arcano.model.Match;
 import com.riccardomalavolti.arcano.model.Player;
 import com.riccardomalavolti.arcano.repositories.MatchRepository;
+import com.riccardomalavolti.arcano.repositories.PlayerRepository;
 
 public class MatchService {
 
+	@Inject
 	private MatchRepository matchRepo;
+	
+	@Inject
+	private PlayerRepository playerRepo;
 
-	public Match createMatch(Player playerOne, Player playerTwo) {
-		Match m = new Match();
-		m.setPlayerOne(playerOne);
-		m.setPlayerTwo(playerTwo);
-		return matchRepo.addMatch(m);
+	public Match createMatch(Match match) {
+		Player p1 = playerRepo.getPlayerById(
+				match.getPlayerOne().getId())
+		.orElseThrow(() -> new NotFoundException("No player exist with id" + match.getPlayerOne().getId()));
+		System.out.println("REPO" + p1);
+		match.setPlayerOne(p1);
+				
+		Player p2 = playerRepo.getPlayerById(match.getPlayerTwo().getId())
+				.orElseThrow(() -> new NotFoundException("No player exist with id" + match.getPlayerTwo().getId()));
+		System.out.println("REPO" + p2);
+		match.setPlayerTwo(p2);
+		
+		return matchRepo.addMatch(match);
 	}
 
+	public List<Match> getAllMatches() {
+		return matchRepo.getAllMatches();
+	}
 }
