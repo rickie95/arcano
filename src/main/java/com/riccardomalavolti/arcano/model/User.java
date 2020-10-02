5855f1a02dc63afe642e8eb5bb1dd0cba001523f
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-10-02 11:24:33 +0200

    PLayer -> User

--- src/main/java/com/riccardomalavolti/arcano/model/User.java
+++ src/main/java/com/riccardomalavolti/arcano/model/User.java
@@ -3,49 +3,73 @@ package com.riccardomalavolti.arcano.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 
 @Entity
-public class User {
+public class Player {
 	
 	@Id
 	@GeneratedValue
 	private Long id;
-
-	private String username;
-	private String password;
 	
-	private Role role;
+	@OneToOne
+	@NotNull
+	private User user;
+	
+	private String username;
+	
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
 	
 	public void setId(Long id) {
 		this.id = id;
 	}
 	
-	public Long getId() {
-		return this.id;
-	}
-	
-	public void setRole(Role role) {
-		this.role = role;
-	}
-	
-	public Role getRole() {
-		return this.role;
-	}
-	
 	public String getUsername() {
 		return username;
 	}
-
+	
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
-	public String getPassword() {
-		return password;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
-
-	public void setPassword(String password) {
-		this.password = password;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Player other = (Player) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Player [id=" + id + ", username=" + user.getUsername() + "]";
 	}
 
 }
