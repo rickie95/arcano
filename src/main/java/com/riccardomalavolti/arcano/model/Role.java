7a72a94e6ec38188d83cfe3453b06c80e52718a8
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-10-05 15:45:45 +0200

    Redesign for Role enum, due to authorization annotation's signatures

--- src/main/java/com/riccardomalavolti/arcano/model/Role.java
+++ src/main/java/com/riccardomalavolti/arcano/model/Role.java
@@ -1,7 +1,17 @@
 package com.riccardomalavolti.arcano.model;
 
 public enum Role {
-	ADMINISTRATOR,
-	PLAYER,
-	JUDGE
+	ADMINISTRATOR(Values.ADMIN_VALUE),
+	JUDGE(Values.JUDGE_VALUE),
+	PLAYER(Values.PLAYER_VALUE);
+	
+	
+	Role(String value){}
+
+	public static class Values {
+		public static final String PLAYER_VALUE = "PLAYER";
+		public static final String ADMIN_VALUE = "ADMINISTRATOR";
+		public static final String JUDGE_VALUE = "JUDGE";
+	}
+    
 }
