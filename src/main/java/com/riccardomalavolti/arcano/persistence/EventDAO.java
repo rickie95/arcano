d35d974ebdf28f9016c8eb16ef1144d6a861ba51
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-10-16 16:13:13 +0200

    Fixs typo in SQL query

--- src/main/java/com/riccardomalavolti/arcano/persistence/EventDAO.java
+++ src/main/java/com/riccardomalavolti/arcano/persistence/EventDAO.java
@@ -8,7 +8,7 @@ import com.riccardomalavolti.arcano.model.Event;
 
 public class EventDAO extends MySQLGenericDAO<Event> {
 
-	public static final String SELECT_ALL_EVENTS = "FROM Event;";
+	public static final String SELECT_ALL_EVENTS = "FROM Event";
 	
 	public EventDAO() {
 		super.setClass(Event.class);
