c5a8c513fcb207ce24cb73f9a06f5a41cd7cf802
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-11-17 15:20:44 +0100

    Increases coverage for EventService

--- src/test/java/com/riccardomalavolti/arcano/service/EventServiceTest.java
+++ src/test/java/com/riccardomalavolti/arcano/service/EventServiceTest.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 import javax.ws.rs.NotFoundException;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
@@ -36,20 +37,23 @@ import com.riccardomalavolti.arcano.repositories.EventRepository;
 @RunWith(JUnitPlatform.class)
 class EventServiceTest {
 	
+	Event eventOne;
+	Event eventTwo;
+	
 	@Mock EventRepository eventRepo;
 	@Mock UserService userService;
 	@Mock AuthorizationService authService;
 	
 	@InjectMocks EventService eventService;
 	
+	@BeforeEach
+	void setupResources() {
+		eventOne = new Event((long)1);
+		eventTwo = new Event((long) 2);
+	}
+	
 	@Test
 	void testGetAllEventShouldRetrunAListOfEvents() {
-		Event eventOne = new Event();
-		eventOne.setId((long)1);
-		
-		Event eventTwo = new Event();
-		eventTwo.setId((long) 2);
-		
 		List<Event> eventList = new ArrayList<Event>(Arrays.asList(eventOne, eventTwo));
 		when(eventRepo.getAllEvents()).thenReturn(eventList);
 		
@@ -71,10 +75,7 @@ class EventServiceTest {
 	}
 	
 	@Test
-	void testGetEventById() {
-		Event eventOne = new Event();
-		eventOne.setId((long)1);
-		
+	void testGetEventById() {		
 		when(eventRepo.getEventById(eventOne.getId())).thenReturn(Optional.of(eventOne));
 		
 		EventDetails result = eventService.getEventById(eventOne.getId());
@@ -92,34 +93,60 @@ class EventServiceTest {
 	}
 	
 	@Test
+	void testCreateEvent() {
+		when(eventRepo.addEvent(eventOne)).thenReturn(eventOne);
+		EventDetails eventCreated = eventService.createEvent(eventOne);
+		assertThat(eventCreated.getId()).isEqualTo(eventOne.getId());
+		}
+	
+	@Test
+	void testRemoveAnEvent() {
+		when(eventRepo.getEventById(eventOne.getId())).thenReturn(Optional.of(eventOne));
+		when(eventRepo.removeEvent(eventOne)).thenReturn(eventOne);
+		EventDetails removedEvent = eventService.removeEvent(eventOne.getId(), "ALLOWED");
+		
+		assertThat(removedEvent).isNotNull();
+		assertThat(removedEvent.getId()).isEqualTo(eventOne.getId());
+	}
+	
+	@Test
+	void testUpdateEvent() {
+		Event toBeUpdated = new Event();
+		when(eventRepo.getEventById(eventOne.getId())).thenReturn(Optional.of(eventOne));
+		when(eventRepo.updateEvent(toBeUpdated)).thenReturn(toBeUpdated);
+		
+		EventDetails updatedEvent = eventService.updateEvent(eventOne.getId(), toBeUpdated, "ALLOWED");
+		
+		assertThat(updatedEvent.getId()).isEqualTo(eventOne.getId());
+	}
+		
+	@Test
 	void testEnrollPlayerInAEvent() {
 		User user = new User();
 		user.setId((long)1);
 		
-		Event event = new Event((long)2);
-		event.enrollPlayer(user);
+		eventOne.enrollPlayer(user);
 		
-		when(eventRepo.getEventById(event.getId())).thenReturn(Optional.of(event));
+		when(eventRepo.getEventById(eventOne.getId())).thenReturn(Optional.of(eventOne));
 		
-		EventDetails result = eventService.enrollPlayerInEvent(user.getId(), event.getId());
+		EventDetails result = eventService.enrollPlayerInEvent(user.getId(), eventOne.getId());
 		
-		assertThat(result.getId()).isEqualTo(event.getId());
+		assertThat(result.getId()).isEqualTo(eventOne.getId());
 		assertThat(result.getPlayerList()).contains(UserMapper.toUserBrief(user));
 	}
 	
 	@Test
 	void testEnrollPlayerInEventShouldThrowNotFoundExceptionIfUserCantBeFound() {
+		Long eventId = eventOne.getId();
 		Long userId = (long)1;
 		User user = new User();
 		user.setId(userId);
 		
-		Event event = new Event((long)2);
-		
-		when(eventRepo.getEventById(event.getId())).thenReturn(Optional.of(event));
+		when(eventRepo.getEventById(eventOne.getId())).thenReturn(Optional.of(eventOne));
 		when(userService.getUserById(user.getId())).thenThrow(NotFoundException.class);
 		
 		assertThatThrownBy(
-				() -> eventService.enrollPlayerInEvent(userId, (long)2)
+				() -> eventService.enrollPlayerInEvent(userId, eventId)
 			).isInstanceOf(NotFoundException.class);
 	}
 	
@@ -138,18 +165,16 @@ class EventServiceTest {
 	
 	@Test
 	void testGetJudgeListShouldReturnJudgeBriefList() {
-		Long eventId = (long) 1;
-		Event event = new Event(eventId);
 		User judgeOne = new User((long) 2);
 		judgeOne.setRole(Role.JUDGE);
 		User judgeTwo = new User((long) 3);
 		judgeTwo.setRole(Role.JUDGE);
-		event.addJudge(judgeOne);
-		event.addJudge(judgeTwo);
+		eventOne.addJudge(judgeOne);
+		eventOne.addJudge(judgeTwo);
 		
-		when(eventRepo.getEventById(eventId)).thenReturn(Optional.of(event));
+		when(eventRepo.getEventById(eventOne.getId())).thenReturn(Optional.of(eventOne));
 		
-		List<UserBrief> judgeList = eventService.getJudgeList(eventId); 
+		List<UserBrief> judgeList = eventService.getJudgeList(eventOne.getId()); 
 		assertThat(judgeList).contains(
 				UserMapper.toUserBrief(judgeOne), 
 				UserMapper.toUserBrief(judgeTwo));
@@ -157,44 +182,54 @@ class EventServiceTest {
 	
 	@Test
 	void testGetJudgeListShouldReturnAnEmptyListIfNoJudgesArePresent() {
-		Long eventId = (long) 1;
-		Event event = new Event(eventId);
+		when(eventRepo.getEventById(eventOne.getId())).thenReturn(Optional.of(eventOne));
 		
-		when(eventRepo.getEventById(eventId)).thenReturn(Optional.of(event));
-		
-		List<UserBrief> judgeList = eventService.getJudgeList(eventId); 
+		List<UserBrief> judgeList = eventService.getJudgeList(eventOne.getId()); 
 		assertThat(judgeList).isEmpty();
 	}
 	
 	@Test
 	void testEnrollJudgeInEventShouldReturnJudgeBrief() {
 		Long judgeId = (long) 1;
-		Long eventId = (long) 2;
-		Event event = new Event(eventId);
 		User judge = new User(judgeId);
 		judge.setRole(Role.JUDGE);
-		when(eventRepo.getEventById(eventId)).thenReturn(Optional.of(event));
+		when(eventRepo.getEventById(eventOne.getId())).thenReturn(Optional.of(eventOne));
 		when(userService.getUserWithRoleById(judgeId, Role.JUDGE)).thenReturn(judge);
 		
-		UserDetails returnedJudge = eventService.enrollJudgeInEvent(judgeId, eventId, "ADMIN");
+		UserDetails returnedJudge = eventService.enrollJudgeInEvent(judgeId, eventOne.getId(), "ADMIN");
 		
 		assertThat(returnedJudge).isEqualTo(UserMapper.toUserDetails(judge));
 		assertThat(returnedJudge.getId()).isEqualTo(judgeId);
-		assertThat(event.getJudgeList()).contains(judge);
+		assertThat(eventOne.getJudgeList()).contains(judge);
 	}
 	
 	@Test
 	void testEnrollJudgeShouldThrowAnExceptionIfUserIsNotJudge() {
 		Long judgeId = (long) 1;
-		Long eventId = (long) 2;
-		Event event = new Event(eventId);
+		Long eventId = eventOne.getId();
 		
-		when(eventRepo.getEventById(eventId)).thenReturn(Optional.of(event));
+		when(eventRepo.getEventById(eventOne.getId())).thenReturn(Optional.of(eventOne));
 		when(userService.getUserWithRoleById(judgeId, Role.JUDGE)).thenThrow(IllegalArgumentException.class);
 		
 		assertThatThrownBy(() -> eventService.enrollJudgeInEvent(judgeId, eventId, "ALLOWED"))
 			.isInstanceOf(IllegalArgumentException.class);
-		verify(authService).verifyOwnershipOf(event, "ALLOWED");
+		verify(authService).verifyOwnershipOf(eventOne, "ALLOWED");
+	}
+	
+	@Test
+	void testGetPlayerList() {
+		User playerOne = new User((long) 2);
+		playerOne.setRole(Role.PLAYER);
+		User playerTwo = new User((long) 3);
+		playerTwo.setRole(Role.PLAYER);
+		eventOne.enrollPlayer(playerOne);
+		eventOne.enrollPlayer(playerTwo);
+		
+		when(eventRepo.getEventById(eventOne.getId())).thenReturn(Optional.of(eventOne));
+		
+		List<UserBrief> playerList = eventService.getPlayersForEvent(eventOne.getId());
+		
+		assertThat(playerList).contains(UserMapper.toUserBrief(playerOne), UserMapper.toUserBrief(playerTwo));
 	}
 
 }
