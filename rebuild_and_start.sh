8bcae0dd6e1100ac14b5e22c769d4c5bf0b60ed4
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2021-10-13 17:53:11 +0200

    error handling for rebuild and start script

--- rebuild_and_start.sh
+++ rebuild_and_start.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
 
 mvn clean package -DskipTest=true
+if [[ $? -ne 0 ]]; then
+    echo "Maven build failed"
+    exit 1
+fi
+
 docker-compose build
+if [[ $? -ne 0 ]]; then
+    echo "Docker build failed"
+    exit 2
+fi
+
 docker-compose up
